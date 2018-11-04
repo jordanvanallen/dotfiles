8a4156196a6c941f70f71d81dfa900c6740cc6b1
Jordan Van Allen <jordanvanallen@pm.me>
2018-11-04 16:58:12 -0500

    polybar configs from szorfein

--- config/.config/polybar/launch.sh
+++ config/.config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar bar1 &
-polybar bar2 &
+polybar top &
+polybar top-second &
 
 echo "Polyar launched..."
