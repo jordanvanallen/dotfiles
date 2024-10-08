2ac45b4357c10b93df0f06e7ee91afb3baabf5f8
Jordan Van Allen <jordanvanallen@pm.me>
2024-10-08 19:14:05 -0400

    cleanup and add hypr

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "Mofiqul/dracula.nvim" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    variant = "moon",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine-moon",
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "dracula",
+    },
+  },
+  -- {
+  --   "catppuccin/nvim",
+  --   opts = {
+  --     background = { -- :h background
+  --       light = "latte",
+  --       dark = "mocha",
+  --     },
+  --     transparent_background = false,
+  --   },
+  -- },
+}
